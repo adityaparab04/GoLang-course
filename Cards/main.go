2099e05e1f48af69c57f8f1109d80df230ca15db
adityaparab04 <adityaparab04@gmail.com>
2024-04-19 17:37:20 -0400

    toString function

--- Cards/main.go
+++ Cards/main.go
@@ -3,16 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	// cards := newDeck()
-	
-	// hand, remCards := deal(cards, 5)
-
-	// hand.print()
-	// remCards.print()
-
-	greeting := "Hello World"
-
-	fmt.Println([]byte(greeting))
-		
+	cards := newDeck()
+	fmt.Println(cards.toString())
 }
